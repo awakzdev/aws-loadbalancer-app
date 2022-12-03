a1e848f7f761cd880040244867c604f1a248399b
Elazar <96201125+awakzdev@users.noreply.github.com>
2022-12-03 23:53:29 +0200

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,4 @@
+output "domain" { value = "https://${aws_acm_certificate.ssl.domain_name}" }
+output "ec2" { value = { "EC2-ubuntu-one" : aws_instance.ec2["vpc-subnet-one"].public_ip, "EC2-ubuntu-two" : aws_instance.ec2["vpc-subnet-two"].public_ip } }
+output "alb" { value = aws_lb.alb.dns_name }
+output "endpoint" { value = aws_db_instance.db.endpoint }
