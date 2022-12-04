df7f8d0713212d4658b6cb556fd22d1e25829307
Elazar <96201125+awakzdev@users.noreply.github.com>
2022-12-04 11:11:50 +0200

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "domain" { value = "https://${aws_acm_certificate.ssl.domain_name}" }
 output "endpoint" { value = aws_db_instance.db.endpoint }
 output "alb" { value = aws_lb.alb.dns_name }
-output "ec2" { value = { "EC2-ubuntu-one" : aws_instance.ec2["vpc-subnet-one"].public_ip, "EC2-ubuntu-two" : aws_instance.ec2["vpc-subnet-two"].public_ip } }
+output "ec2" { value = { for k, v in aws_instance.ec2: k => v.public_ip } }
