998821184ebfdda132cd01bc89bd58984276dffc
Elazar <96201125+awakzdev@users.noreply.github.com>
2023-02-22 10:47:54 +0200

    Update variables.tf

--- staging-example/variables.tf
+++ staging-example/variables.tf
@@ -87,13 +87,3 @@ variable "database_name" {
   type        = string
   description = "Naming convention for Database"
 }
-
-variable "route53_zone" {
-  type        = string
-  description = "Route53 zone name pointing to your domain - domain.com"
-}
-
-variable "sub_domain" {
-  type        = string
-  description = "Your subdomain - foo.domain.com"
-}
