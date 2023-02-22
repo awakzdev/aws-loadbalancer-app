958fdcbd51227e52667d1bd939ee175c2e4500cc
Elazar <96201125+awakzdev@users.noreply.github.com>
2023-02-22 08:13:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -227,41 +227,9 @@ resource "aws_security_group" "ec2_sg" {
   }
 }
 
-
-####################
-#  Certificate - ACM
-####################
-
-# ACM Certificate issue
-resource "aws_acm_certificate" "ssl" {
-  domain_name       = "modules.cclab.cloud-castles.com"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = {
-    Environment = "test"
-  }
-}
-
-# ACM Certificate validation
-resource "aws_acm_certificate_validation" "example" {
-  certificate_arn         = aws_acm_certificate.ssl.arn
-  validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
-}
-
-
 ####################
 #   Route 53 - DNS
 ####################
-
-# Route 53 Zone
-data "aws_route53_zone" "example" {
-  name         = "cclab.cloud-castles.com"
-  private_zone = false
-}
 
 # Route 53 - Certificate Record
 resource "aws_route53_record" "example" {
@@ -280,20 +248,6 @@ resource "aws_route53_record" "example" {
   type            = each.value.type
   zone_id         = data.aws_route53_zone.example.zone_id
 }
-
-# Route 53 - ALB Alias
-resource "aws_route53_record" "alias_route53_record" {
-  zone_id = data.aws_route53_zone.example.zone_id
-  name    = "modules.cclab.cloud-castles.com"
-  type    = "A"
-
-  alias {
-    name                   = aws_lb.alb.dns_name
-    zone_id                = aws_lb.alb.zone_id
-    evaluate_target_health = true
-  }
-}
-
 
 ####################
 #   Load Balancer
